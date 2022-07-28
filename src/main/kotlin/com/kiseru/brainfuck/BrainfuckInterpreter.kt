c92c8338ce270398fd6e97c065a6efaeb117ebce
Alexandr Kiselev <kis.alex97@gmail.com>
2022-07-28 22:06:10 +0300

    creating reader by nio

--- src/main/kotlin/com/kiseru/brainfuck/BrainfuckInterpreter.kt
+++ src/main/kotlin/com/kiseru/brainfuck/BrainfuckInterpreter.kt
@@ -1,15 +1,16 @@
 package com.kiseru.brainfuck
 
-import java.io.BufferedReader
-import java.io.FileReader
+import java.nio.file.Files
+import java.nio.file.Paths
 
 /**
  * Размерность ленты.
  */
-private const val TAPE_LENGTH = 30000;
+private const val TAPE_LENGTH = 30000
 
 fun main(args: Array<String>) {
-    val filename = args[0]
-    val brainfuck = Brainfuck(CharArray(TAPE_LENGTH), BufferedReader(FileReader(filename)))
+    val filename = Paths.get(args[0])
+    val bufferedReader = Files.newBufferedReader(filename)
+    val brainfuck = Brainfuck(CharArray(TAPE_LENGTH), bufferedReader)
     brainfuck.run()
 }
