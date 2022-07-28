954176595bb64a4f72b65bedae793fb1348c786f
Alexandr Kiselev <kis.alex97@gmail.com>
2022-07-28 22:10:35 +0300

    added closing reader

--- src/main/kotlin/com/kiseru/brainfuck/BrainfuckInterpreter.kt
+++ src/main/kotlin/com/kiseru/brainfuck/BrainfuckInterpreter.kt
@@ -10,7 +10,8 @@ private const val TAPE_LENGTH = 30000
 
 fun main(args: Array<String>) {
     val filename = Paths.get(args[0])
-    val bufferedReader = Files.newBufferedReader(filename)
-    val brainfuck = Brainfuck(CharArray(TAPE_LENGTH), bufferedReader)
-    brainfuck.run()
+    Files.newBufferedReader(filename).use {
+        val brainfuck = Brainfuck(CharArray(TAPE_LENGTH), it)
+        brainfuck.run()
+    }
 }
